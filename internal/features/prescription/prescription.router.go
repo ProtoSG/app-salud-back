ce8dc9058800f96d3fa239ae462882c58b556b9e
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-05 16:08:55 -0500

    feat: add prescription router

---
+++ internal/features/prescription/prescription.router.go
@@ -0,0 +1,13 @@
+package prescription
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewRouter(r *mux.Router, svc *Service) {
+	controller := NewController(svc)
+
+	r.Handle("/prescription", http.HandlerFunc(controller.Register)).Methods("POST")
+}
