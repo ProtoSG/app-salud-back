87a34fc68ed9d90634941b71617865061ff44c0f
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-05 16:42:52 -0500

    fix: implement authorization for methods

--- internal/features/prescription/prescription.router.go
+++ internal/features/prescription/prescription.router.go
@@ -3,12 +3,13 @@ package prescription
 import (
 	"net/http"
 
+	"github.com/ProtoSG/app-salud-back/internal/middleware"
 	"github.com/gorilla/mux"
 )
 
 func NewRouter(r *mux.Router, svc *Service) {
 	controller := NewController(svc)
 
-	r.Handle("/prescription", http.HandlerFunc(controller.GetAll)).Methods("GET")
-	r.Handle("/prescription", http.HandlerFunc(controller.Register)).Methods("POST")
+	r.Handle("/prescription", middleware.Auth(http.HandlerFunc(controller.GetAll))).Methods("GET")
+	r.Handle("/prescription", middleware.Auth(http.HandlerFunc(controller.Register))).Methods("POST")
 }
