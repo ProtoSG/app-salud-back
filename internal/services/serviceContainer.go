c44032327f54825b7ad0d0c5637259447b728107
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-05 02:25:08 -0500

    fix: change usuario for user

--- internal/services/serviceContainer.go
+++ internal/services/serviceContainer.go
@@ -5,23 +5,27 @@ import (
 
 	"github.com/ProtoSG/app-salud-back/internal/features/auth"
 	medicalappointment "github.com/ProtoSG/app-salud-back/internal/features/medicalAppointment"
-	"github.com/ProtoSG/app-salud-back/internal/features/usuario"
+	"github.com/ProtoSG/app-salud-back/internal/features/patient"
+	"github.com/ProtoSG/app-salud-back/internal/features/user"
 )
 
 type ServiceContainer struct {
-	Usuario            *usuario.Service
+	User               *user.Service
 	Auth               *auth.Service
 	MedicalAppointment *medicalappointment.Service
+	Patient            *patient.Service
 }
 
 func NewServiceContainer(db *sql.DB) *ServiceContainer {
-	userRepo := usuario.NewPostgreRepo(db)
+	userRepo := user.NewPostgreRepo(db)
 	authRepo := auth.NewPostgreRepo(db)
 	mAppt := medicalappointment.NewPostgreRepo(db)
+	patientRepo := patient.NewPostgreRepo(db)
 
 	return &ServiceContainer{
-		Usuario:            usuario.NewService(userRepo),
+		User:               user.NewService(userRepo),
 		Auth:               auth.NewService(authRepo),
 		MedicalAppointment: medicalappointment.NewService(mAppt),
+		Patient:            patient.NewService(patientRepo),
 	}
 }
