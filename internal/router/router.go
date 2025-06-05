6f9f9d13e0aa041f5f627103ad47b653c52568c3
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-05 02:24:41 -0500

    feat: add patient router in router container

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,13 +3,15 @@ package router
 import (
 	"github.com/ProtoSG/app-salud-back/internal/features/auth"
 	medicalappointment "github.com/ProtoSG/app-salud-back/internal/features/medicalAppointment"
-	"github.com/ProtoSG/app-salud-back/internal/features/usuario"
+	"github.com/ProtoSG/app-salud-back/internal/features/patient"
+	"github.com/ProtoSG/app-salud-back/internal/features/user"
 	"github.com/ProtoSG/app-salud-back/internal/services"
 	"github.com/gorilla/mux"
 )
 
 func NewRouterContainer(r *mux.Router, svc *services.ServiceContainer) {
-	usuario.NewRouter(r, svc.Usuario)
+	user.NewRouter(r, svc.User)
 	auth.NewRouter(r, svc.Auth)
 	medicalappointment.NewRouter(r, svc.MedicalAppointment)
+	patient.NewRouter(r, svc.Patient)
 }
