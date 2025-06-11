f0ec3693e68ce5d2923663fb2b3e8938116f5282
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-11 16:12:01 -0500

    feat: add middleware required roles for patient endpoints

--- internal/features/patient/patient.router.go
+++ internal/features/patient/patient.router.go
@@ -10,7 +10,25 @@ import (
 func NewRouter(r *mux.Router, svc *Service) {
 	controller := NewController(svc)
 
-	r.Handle("/patient", middleware.Auth(http.HandlerFunc(controller.Create))).Methods("POST")
-	r.Handle("/patient", middleware.Auth(http.HandlerFunc(controller.GetAll))).Methods("GET")
-	r.Handle("/patient/{id}", middleware.Auth(http.HandlerFunc(controller.GetByID))).Methods("GET")
+	r.Handle("/patient",
+		middleware.Auth(
+			middleware.RequireRoles("DOCTOR", "ENFERMERO")(
+				http.HandlerFunc(controller.Create),
+			),
+		),
+	).Methods("POST")
+	r.Handle("/patient",
+		middleware.Auth(
+			middleware.RequireRoles("DOCTOR", "ENFERMERO")(
+				http.HandlerFunc(controller.GetAll),
+			),
+		),
+	).Methods("GET")
+	r.Handle("/patient/{id}",
+		middleware.Auth(
+			middleware.RequireRoles("DOCTOR", "ENFERMERO")(
+				http.HandlerFunc(controller.GetByID),
+			),
+		),
+	).Methods("GET")
 }
