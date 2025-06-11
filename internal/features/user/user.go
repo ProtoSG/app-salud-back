e2a52ba2f991b1e1f2cfd8dfd66c06025e719f87
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-11 16:13:59 -0500

    feat: add example User struct

--- internal/features/user/user.go
+++ internal/features/user/user.go
@@ -1,11 +1,11 @@
 package user
 
 type User struct {
-	RoleID    int    `json:"role_id" validate:"required"`
-	FirstName string `json:"first_name" validate:"required"`
-	LastName  string `json:"last_name" validate:"required"`
-	Email     string `json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required"`
+	RoleID    int    `json:"role_id" validate:"required" example:"1"`
+	FirstName string `json:"first_name" validate:"required" example:"Diego Alberto"`
+	LastName  string `json:"last_name" validate:"required" example:"Salazar Garcia"`
+	Email     string `json:"email" validate:"required,email" example:"diego@gmail.com"`
+	Password  string `json:"password" validate:"required" example:"tuPasswordSecret"`
 }
 
 func NewUser(roleID int, firstName, lastName, email, password string) *User {
